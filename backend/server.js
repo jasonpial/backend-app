d64ea1fe7182722e84a53eda1e921cfcad106a30
jasonpial <jumadavidpius@gmail.com>
2025-11-22 12:49:01 +0300

    Refactor employeeRoutes import in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/employees', require('./routes/employeeRoutes'));
+app.use('/api/employees', employeeRoutes);
 app.use('/api/attendance', require('./routes/attendanceRoutes'));
 app.use('/api/leaves', require('./routes/leaveRoutes'));
 app.use('/api/payroll', require('./routes/payrollRoutes'));
